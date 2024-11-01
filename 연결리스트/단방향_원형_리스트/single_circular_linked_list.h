7c66593c8fd5f35124b395e4107e60716225c2a6
KimKyungWon0519 <gwon080870@gmail.com>
2024-11-02 02:04:14 +0900

    단방향 원형 리스트 생성

---
+++ 연결리스트/단방향_원형_리스트/single_circular_linked_list.h
@@ -0,0 +1,25 @@
+#ifndef __SINGLE_CIRCULAR_LINKED_LIST_H__
+#define __SINGLE_CIRCULAR_LINKED_LIST_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _Node
+{
+    int data;
+    struct _Node *next_node;
+} Node;
+
+Node *create_node(int data);
+void add_node(Node **head, Node *new_node);
+void insert_after_node(Node **node, Node *new_node);
+void remove_node(Node **head, Node *node);
+void delete_node(Node *node);
+Node *get_node_at(Node *head, int index);
+int get_size(Node *head);
+
+void print_node_information(Node *node);
+void print_all_node(Node *head);
+void print_linked_list_information(Node *head);
+
+#endif
